d9dcc3408a67e0eb14ac6c6ac30502b469449e3f
Lucpen <luci9393@hotmail.com>
2022-03-01 16:17:31 +0100

    Changed variable name to make it more clear

--- read_json_multiQC.py
+++ read_json_multiQC.py
@@ -6,7 +6,7 @@ import pandas as pd
 # path_json: path to json MultiQC file
 # main_dict_to_get_data_from: first subset from json file, if info is in summary table "report_general_stats_data" 
 # inf_sel: list or dictionary to use for subset 
-# Example of input for function in dictionary and list form 
+# Example of input for function: 
 # inf_sel={"salmon_quant":['num_processed','num_mapped','percent_mapped',"library_types"], \
 #     "star_fusion":["total_reads", "uniquely_mapped", "uniquely_mapped_percent", "multimapped","multimapped_percent"]}
 inf_sel=["salmon_quant","star_fusion"]
@@ -31,16 +31,16 @@ samp_name=list(sdata[1].keys())[0].split("_")[0]
 outp=pd.DataFrame({samp_name : []})
 
 # Look through the dictionary and subset information required in inf_sel
-for v in sdata:
+for dict_list in sdata:
     if isinstance(inf_sel, list):
         for subsel in inf_sel:
-            if subsel in v:
-                outp=pd.concat([outp,pd.DataFrame.from_dict(v[subsel], orient='index',columns=[samp_name])])
+            if subsel in dict_list:
+                outp=pd.concat([outp,pd.DataFrame.from_dict(dict_list[subsel], orient='index',columns=[samp_name])])
     elif isinstance(inf_sel, dict):
         for key in inf_sel:
-            if key in v:
+            if key in dict_list:
                 keys_to_extract=inf_sel[key]
-                tempd=v[key]
+                tempd=dict_list[key]
                 tempd = {key_ex: tempd[key_ex] for key_ex in keys_to_extract}
                 outp=pd.concat([outp,pd.DataFrame.from_dict(tempd, orient='index',columns=[samp_name])])
     else:
