4d3f8314bb30f625a8c508cb72f31ad2e9430e17
Lucpen <luci9393@hotmail.com>
2022-03-01 16:08:19 +0100

    Scripts now accepts both dictionaries and lists to subset json file

--- read_json_multiQC.py
+++ read_json_multiQC.py
@@ -1,30 +1,52 @@
 import json
 import pandas as pd
 
-# Dictionary you want to get informaiton from
+# Output subset of nested dictionary in json MultiQC file
+# Input:
+# path_json: path to json MultiQC file
+# main_dict_to_get_data_from: first subset from json file, if info is in summary table "report_general_stats_data" 
+# inf_sel: list or dictionary to use for subset 
+# Example of input for function in dictionary and list form 
+# inf_sel={"salmon_quant":['num_processed','num_mapped','percent_mapped',"library_types"], \
+#     "star_fusion":["total_reads", "uniquely_mapped", "uniquely_mapped_percent", "multimapped","multimapped_percent"]}
 inf_sel=["salmon_quant","star_fusion"]
+path_json='/Users/luciapenaperez/Downloads/multiqc_data.json'
+main_dict_to_get_data_from='report_general_stats_data'
 
-# Opening JSON file
-f = open('/Users/luciapenaperez/Downloads/multiqc_data.json')
- 
-# returns JSON object as a dictionary
+f = open(path_json)
+ # returns JSON object as a dictionary
 data = json.load(f)
+
+# Closing data file
+f.close()
 
 # I want to output data within the general report
 # The sdata is a list of dictionaries
-sdata=data['report_general_stats_data']
+sdata=data[main_dict_to_get_data_from]
 
-# Look through the list and select the dictionary to get data from
+# Gettign sample name to have it as column name on output
+samp_name=list(sdata[1].keys())[0].split("_")[0]
+
+# Empty dataframe to concat with results
+outp=pd.DataFrame({samp_name : []})
+
+# Look through the dictionary and subset information required in inf_sel
 for v in sdata:
-    if isinstance(v, dict):
+    if isinstance(inf_sel, list):
         for subsel in inf_sel:
             if subsel in v:
-                #print(v)
-                d3=v[subsel]
-                print(type(d3))
-               # print(d3.keys())
-                print(d3)
-                d4=pd.DataFrame.from_dict(d3, orient='index',columns=[])
-                print(d4)
+                outp=pd.concat([outp,pd.DataFrame.from_dict(v[subsel], orient='index',columns=[samp_name])])
+    elif isinstance(inf_sel, dict):
+        for key in inf_sel:
+            if key in v:
+                keys_to_extract=inf_sel[key]
+                tempd=v[key]
+                tempd = {key_ex: tempd[key_ex] for key_ex in keys_to_extract}
+                outp=pd.concat([outp,pd.DataFrame.from_dict(tempd, orient='index',columns=[samp_name])])
+    else:
+        print("You must input a list or dictionary with the information you want out!")
+
+print(outp)
+
 
 
